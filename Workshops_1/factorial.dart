f88bc3dab375a32394d6e015c1ebe967c393cf4b
Bilal Gönül <92885233+bilal1616@users.noreply.github.com>
2023-10-27 00:55:27 +0300

    Add files via upload

---
+++ Workshops_1/factorial.dart
@@ -0,0 +1,19 @@
+int Factorial(int number) {
+  if (number == 0) {
+    return 1; // 0 faktöriyel 1'dir.
+  }
+
+  int result = 1;
+  for (int i = 1; i <= number; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+void main() {
+  int input =
+      5; // Faktöriyeli hesaplanacak sayı, istediğiniz değeri kullanabilirsiniz.
+  int factorialResult = Factorial(input);
+  print('$input faktöriyeli: $factorialResult');
+  print('$input faktöriyeli: $factorialResult');
+}
---
+++ Workshops_2/armstrongSayi.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+
+void main() {
+  for (int number = 1; number <= 10000; number++) { //for döngüsü, 1 ile 10,000 arasındaki sayıları sırayla kontrol eder. 
+    if (isArmstrongNumber(number)) { //Her döngü adımında, isArmstrongNumber(number) fonksiyonunu kullanarak number değişkenini kontrol eder.
+      print("$number bir Armstrong sayısıdır");
+    }
+  }
+}
+
+bool isArmstrongNumber(int number) {
+  int originalNumber = number; //Bu satır, "number" adlı bir değişkenin orijinal değerini saklar
+  int numberOfDigits = number.toString().length; //"number" sayısını bir metin (string) olarak dönüştürür ve bu metinin uzunluğunu alır
+  int sum = 0; //Bu satır, toplam değişkenini sıfırlar
+
+  while (number > 0) {
+    int digit = number % 10; //en sağdaki rakamı alır. "number % 10" ifadesi, "number" değişkenini 10'a böldüğünde kalanı hesaplar
+    sum += pow(digit, numberOfDigits).toInt(); //Bu satır, "digit" değişkenindeki rakamın "numberOfDigits" üssünü hesaplar ve "sum" adlı bir toplam değişkenine ekler.
+    number ~/= 10; //Bölümün tam sayıya dönüşümü
+  }
+
+  return sum == originalNumber; //"return sum == originalNumber;" ifadesi, sum ve originalNumber adlı iki değişkenin eşit olup olmadığını kontrol eder ve sonucu bir şart ifadesi olarak döndürür
+}
